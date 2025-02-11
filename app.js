39ee0762504f945c31a9e23e8a2f6f76b74d8eac
MielVil <miel.18m@gmail.com>
2025-02-10 18:52:58 -0600

    Lista-amigos

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaNombres =[];
+
+function agregarAmigo(){
+let nombre = document.getElementById('amigo').value.trim();
+
+    if(nombre === ''){
+        alert("Por fevor, inserte un nombre valido");
+    } else{
+        
+        console.log(nombre);
+        let nuevoAmigo = document.createElement("li"); //Crea campo vacio para que se agregue en la lista
+        nuevoAmigo.textContent = nombre;
+        document.getElementById("listaAmigos").appendChild(nuevoAmigo);
+        listaNombres.push(nombre);
+    }
+limpiarCampo();
+
+
+}
+
+function limpiarCampo(){
+    document.getElementById('amigo').value =''; 
+
+}

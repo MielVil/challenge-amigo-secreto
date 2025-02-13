27da299ef39f6da59e2a1c72ccce3b11951c76da
MielVil <miel.18m@gmail.com>
2025-02-13 15:42:25 -0600

    lista-sorteo-de-amigo-secreto

--- app.js
+++ app.js
@@ -1,26 +1,52 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let listaNombres =[];
+const listaNombres =[];
 
 function agregarAmigo(){
-let nombre = document.getElementById('amigo').value.trim();
+    const nombre = document.getElementById('amigo').value.trim();
+    const lista = document.getElementById("listaAmigos");
+    
 
     if(nombre === ''){
         alert("Por fevor, inserte un nombre valido");
     } else{
-        
-        console.log(nombre);
-        let nuevoAmigo = document.createElement("li"); //Crea campo vacio para que se agregue en la lista
-        nuevoAmigo.textContent = nombre;
-        document.getElementById("listaAmigos").appendChild(nuevoAmigo);
         listaNombres.push(nombre);
+        const espacioLista = document.createElement("li"); //Crea campo vacio para que se agregue en la lista
+        espacioLista.textContent = nombre;
+        lista.appendChild(espacioLista);
+        
     }
-limpiarCampo();
+
+  limpiarCampo();
+
+}
+
+// Sorteo
+function sortearAmigo(){
+    
+    if(listaNombres.length === 0){
+            alert("La lista de amigos está vacía. Agrega amigos antes de sortear.");
+    }else{
+        let indexAmigo = Math.floor(Math.random()*listaNombres.length);
+        let tuAmigoSecretoEs = document.getElementById("tuAmigoEs");
+        
+        tuAmigoSecretoEs.innerHTML = `${listaNombres[indexAmigo]}`;
+        listaNombres.splice(indexAmigo,1);
+    }
+
+    
+    
+}
+
+// Verificar que no existan duplicados
 
 
+// reiniciar
+function reiniciarSorteo(){
+    lista.innerHTML="";
+    limpiarCampo(); 
 }
 
 function limpiarCampo(){
     document.getElementById('amigo').value =''; 
-
 }
